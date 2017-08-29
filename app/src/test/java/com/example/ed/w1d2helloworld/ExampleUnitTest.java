5fb1ebfd24bc3b9016fbba3fd9fbaf74b21f8877
sdsearle <spencer.seiro@gmail.com>
2017-08-29 16:01:50 -0400

    Initial commit This is for the Hello World App

---
+++ app/src/test/java/com/example/ed/w1d2helloworld/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.ed.w1d2helloworld;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
